5cc4b821db61cecd72ec9ee4b2604df98fcf4677
Amir Hosein Eskandani <40829778+aheskandani@users.noreply.github.com>
2022-07-15 13:30:11 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,20 @@
-import logging
-import os
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
 from instaloader import Instaloader, Profile
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-PORT = int(os.environ.get('PORT', 5000))
 bot_token = '5449413191:AAHq9zYO6Zj23lj2vNY8jRmhHXC2VNTHeMc'
+updater = Updater(bot_token, use_context=True)
 
-def start(update, context):
-	welcome_text = 'Hello friend to instagram profile save bot 🌹\n
-		        Please enter the username or url and wait to download 🙏🏻\n\n
-			Developed by @eskandani'
-	update.message.reply_text(welcome_text)
+def start(update: Update, context: CallbackContext):
+	update.message.reply_text('Hello friend to instagram profile save bot 🌹\nPlease enter the username or url and wait to download 🙏🏻')
 
-def id(update, context):
+def id(update: Update, context: CallbackContext):
 	update.message.reply_text(f'Chat ID: {update.message.chat_id}')
 
-def error(update, context):
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
-	
 def url_to_username(url):
 	if len(url) > 30:
 		website = 'instagram.com/'
@@ -28,8 +23,8 @@ def url_to_username(url):
 		return url[start_idx:end_idx]
 	else:
 		return url
-
-def download_pic(update, context):
+    
+def unknown_text(update: Update, context: CallbackContext):
     try:
         insta = Instaloader()
         username = url_to_username(update.message.text)
@@ -40,17 +35,21 @@ def download_pic(update, context):
     except:
 	    update.message.reply_text('User not found!')
 
-updater = Updater(bot_token, use_context=True)
+def download_pic(update: Update, context: CallbackContext):
+    try:
+        insta = Instaloader()
+        username = url_to_username(update.message.text)
+        profile = Profile.from_username(insta.context, username)
+        url = profile.get_profile_pic_url()
+        cap = profile.full_name + '\n\n' + profile.biography + '\n\n' + '@instaprofilesave_bot'
+        update.message.reply_photo(photo=url, caption=cap)
+    except:
+	    update.message.reply_text('User not found!')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('id', id))
 updater.dispatcher.add_handler(MessageHandler(Filters.text, download_pic))
 updater.dispatcher.add_handler(MessageHandler(Filters.command, download_pic))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, download_pic))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
 
-dp.add_error_handler(error)
-updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-updater.bot.setWebhook('https://obscure-chamber-30229.herokuapp.com/' + bot_token)
-updater.idle()
+updater.start_polling()
